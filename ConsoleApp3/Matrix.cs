abc9ee79fcd92d2ca6f9bef1972ba0a05b1dc5af
aseallotfe <133778455+aseallotfe@users.noreply.github.com>
2023-05-18 00:28:38 +0300

    Add files via upload

---
+++ ConsoleApp3/Matrix.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Matrix
+    {
+
+        private int[,] re;
+        private int x;
+        private int y;
+        public Matrix(int x,int y)
+        {
+            this.x = x;
+            this.y = y;
+            re = new int[x, y]; 
+
+        }
+        public void ReadMatrix() 
+        {
+            Console.WriteLine("enter the elements");
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    re[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+        }
+        public void Transpose()
+        {
+           int[,] trre = new int[x, y];
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j <y;j ++)
+                {
+                    trre[j,i] = re[i, j];
+
+                }
+            } re = trre;
+            
+            int temp = x;
+            x = y;
+            y = temp;
+          
+        } 
+        public void print()
+        {
+            Console.WriteLine("the elements is");
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    Console.WriteLine($"{re[i, j]}");
+                }
+                
+            }
+
+        }
+
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp3;
+using System;
+using System.Runtime.CompilerServices;
+Console.WriteLine("enter the number of rows");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter the number of columns");
+int y = Convert.ToInt32(Console.ReadLine());
+Matrix str = new Matrix(x,y);
+str.ReadMatrix( );
+str.print();
+str.Transpose();
+str.print();
